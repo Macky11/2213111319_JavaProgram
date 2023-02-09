4d6a254882ec643b9c6df10e696f5ac653301f44
Macky11 <2213111319@TNISTUDENT.AC.TH>
2023-02-09 14:58:45 +0700

    Pratice1

---
+++ OOP_Programe/src/cylinder.java
@@ -0,0 +1,29 @@
+public class cylinder extends circle{
+
+    private double height;
+    
+    public cylinder(double radius, double height) {
+        super(null, radius);
+        this.height = height;
+    }
+
+    public double getHeight(){
+        return this.height;
+    }
+    public double getVolume(){
+        return super.getArea() * getHeight();
+    }
+
+    @Override
+    public double getArea() {
+        return 2 * Math.PI * getHeight() * getRadius();
+    }
+
+    @Override
+    public String toString() {
+        return "height = " + getHeight() + "," + super.toString();
+    }
+
+
+
+}
